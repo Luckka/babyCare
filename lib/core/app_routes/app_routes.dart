0be76b3090b4d24e3e1bd64ee444d9b954fbe380
unknown <luckadev.silva@gmail.com>
2023-12-19 13:58:22 -0300

    feature: start project

---
+++ lib/core/app_routes/app_routes.dart
@@ -0,0 +1,6 @@
+class AppRoutes {
+  static const login = '/login';
+  static const onboardingStep01 = '/onboardingStep01';
+  static const onboardingStep02 = '/onboardingStep02';
+  static const onboardingStep03 = '/onboardingStep03';
+}
